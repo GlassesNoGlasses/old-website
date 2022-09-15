99aabedba732155de96d3b36481bcd11102ea0ba
Neil Wang <neilywang3@gmail.com>
2022-09-15 16:38:56 -0400

    changes 1

---
+++ src/components/middle/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Loader = (props) => {
+  return (
+    <div>
+        <div className='cup'>
+            <div className='handle'></div>
+        </div>
+    </div>
+  )
+}
+
