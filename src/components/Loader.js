216983c0e06ab2f379d8500241f79b13a5dc54da
Neil Wang <neilywang3@gmail.com>
2022-09-20 18:53:44 -0400

    main2

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+
+import './loader.css'
+
+export const Loader = (props) => {
+  return (
+    <div id = "" className='loader-container'>
+      <div className='cup'>
+        <div className='handle'></div>
+      </div>
+    </div>
+  )
+}
+
