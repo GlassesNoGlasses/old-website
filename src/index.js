2d0852e1dd00db550dde94203dfb593ee0a0e500
GlassesNoGlasses <neilywang3@gmail.com>
2022-09-22 16:39:23 -0400

    loadingscreen

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+document.getElementsByClassName("cup")[0].addEventListener("animationend", () => {
+  document.getElementsByClassName("loader-container")[0].id = "endAnim";
+  document.getElementById("endAnim").classList.remove("loader-container");
+}, false);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
