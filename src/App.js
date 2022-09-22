be8ceba36fdb6dc00fdd98d086bff1f11e31b56e
GlassesNoGlasses <neilywang3@gmail.com>
2022-09-22 16:39:40 -0400

    loadingscreen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Header } from "./components/top/Header";
-import { Body } from "./components/middle/Body";
 import './app.css'
-
 
 function App() {
   return (
     <div className="main">
       <div className="top-container">
-        <Header title="Poopity Scoop" style={{ position: 'relative', zIndex: '2' }}/>
+        <Header title="Poopity Scoop"/>
       </div>
       <div className="body-container">
       </div>
