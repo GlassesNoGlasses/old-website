a0964ddc7ec77c26280b6c6fb8ea9b4a10791c91
GlassesNoGlasses <neilywang3@gmail.com>
2022-10-03 22:41:57 -0400

    javaIcon

--- src/components/top/Languages.js
+++ src/components/top/Languages.js
@@ -4,12 +4,14 @@ import python from '../../images/python.svg'
 import javascript from '../../images/javascript.svg'
 import c from '../../images/c.svg'
 import react from '../../images/react.svg'
+import java from '../../images/java.png'
 import './icons.css'
 
 export const Languages = (props) => {
   return (
     <div className='icon-container'>
         <img className='icon-languages' src={props.lng1} alt='1'></img>
+        <img className='icon-languages' src={props.lng5} alt='5'></img>
         <img className='icon-languages' src={props.lng2} alt='2'></img>
         <img className='icon-languages' src={props.lng3} alt='3'></img>
         <img className='icon-languages' src={props.lng4} alt='4'></img>
@@ -22,6 +24,7 @@ Languages.propTypes = {
     lng2: PropTypes.string,
     lng3: PropTypes.string,
     lng4: PropTypes.string,
+    lng5: PropTypes.string,
 }
 
 // default value for props
@@ -30,4 +33,5 @@ Languages.defaultProps = {
     lng2: react,
     lng3: c,
     lng4: python,
+    lng5: java
 }
